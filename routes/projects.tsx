20301350452c2f4d560218f83f01dc88754447a0
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-03-10 09:07:01 +0100

    simplify projects list

--- routes/projects.tsx
+++ routes/projects.tsx
@@ -1,13 +1,19 @@
-export default function Projects() {
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { ProjectCard } from "../components/ProjectCard.tsx";
+import { getProjects, Project } from "../utils/projects.ts";
+
+export const handler: Handlers<Project[]> = {
+  async GET(_req, ctx) {
+    const project = getProjects();
+    return ctx.render(project);
+  },
+};
+
+export default function Projects(props: PageProps<Project[]>) {
+  const projects = props.data;
   return (
     <div>
-      {projects.map((project) => (
-        <div key={project.repo}>
-          <a href={project.link}>{project.repo}</a>
-          <p>{project.image}</p>
-          <p>{project.description}</p>
-        </div>
-      ))}
+      {projects.map((project) => <ProjectCard project={project} />)}
     </div>
   );
 }
