81acf3b76bea15f3da653956043fc4be38a2c63f
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-03-13 22:42:12 +0100

    add resume button and missing imports

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { useState } from "preact/hooks";
-import LemonIcon from "https://deno.land/x/tabler_minor stufficons_tsx@0.0.3/tsx/lemon-2.tsx";
+// import LemonIcon from "https://deno.land/x/tabler_minor stufficons_tsx@0.0.3/tsx/lemon-2.tsx";
 import { Button } from "./Button.tsx";
+import { asset } from "$fresh/runtime.ts";
 export default function Header() {
   // const [isOpen, setIsOpen] = useState(false);
   const menus = [
@@ -10,28 +11,31 @@ export default function Header() {
   ];
 
   return (
-    <div className="mx-auto w-full max-w-5xl py-3 px-3 md:flex md:justify-between md:py-3.5 md:px-4">
-      <div className="flex items-center flex-1">
-        <LemonIcon aria-hidden="true" />
+    <div className="mx-auto w-full max-w-5xl py-3 px-3  md:py-3.5 md:px-4">
+      <div className="flex  flex-row   justify-between">
+        {/* <LemonIcon aria-hidden="true" /> */}
         <div className="text-2xl ml-1 font-bold">
           <a href="/">Home</a>
         </div>
-      </div>
 
-      <div>
-        {" "}
-        {menus.map((menu) => (
-          <div>
-            <a
-              href={menu.href}
-              class="flex p-4 text-gray-500 hover:text-black py-1 border-gray-500 [aria-current]:text-green-600"
-            >
-              {menu.name}
-            </a>
-          </div>
-        ))}{" "}
+        <div>
+          {menus.map((menu) => (
+            <div>
+              <a
+                href={menu.href}
+                class="flex justify-center p-4 text-gray-500  hover:text-black py-1 border-gray-500 [aria-current]:text-green-600"
+              >
+                {menu.name}
+              </a>
+            </div>
+          ))}
+        </div>
+        <div>
+          <Button>
+            <a href={asset("/resume.pdf")}>Resume</a>
+          </Button>
+        </div>
       </div>
-      <Button>Resume</Button>
     </div>
   );
 }
