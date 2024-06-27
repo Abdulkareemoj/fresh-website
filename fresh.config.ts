40f8e803125ddc4bbf530388ac60dc3665f2c024
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-06-27 12:15:47 +0100

    chore: Update fresh.config.ts and _app.tsx files

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
-import { freshSEOPlugin } from "https://deno.land/x/fresh_seo/mod.ts";
+import { freshSEOPlugin } from "https://deno.land/x/fresh_seo@1.0.1/mod.ts";
 import manifest from "./fresh.gen.ts";
 export default defineConfig({
   plugins: [
     tailwind(),
     freshSEOPlugin(manifest, {
       include: [
-        "/blog/index",
+        "/blog",
         "/projects",
         "/contact",
         "/uses",
