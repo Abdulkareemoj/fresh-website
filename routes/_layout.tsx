3266292c08b2b084c71e10406f51cfce5b76fd83
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-07-13 14:55:06 +0100

    feat: Add MobileNav component for responsive navigation menu

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -1,11 +1,13 @@
 import { PageProps } from "$fresh/server.ts";
 import Footer from "../components/Footer.tsx";
 import Header from "../components/Header.tsx";
+import MobileNav from "../components/MobileNav.tsx";
 
 export default function Layout({ Component, state }: PageProps) {
   // do something with state here
   return (
     <>
+      <MobileNav />
       <Header />
       <main className="mx-auto max-w-3xl px-6 py-12 sm:px-6 sm:py-10 lg:py-10">
         <Component />
