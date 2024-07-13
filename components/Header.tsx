468109ec00f6f536807bbad8fbb82ffaa40050ec
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-07-13 14:54:34 +0100

    Update Header component to include responsive navigation menu

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,30 +22,33 @@ const links = [
 
 export default function Header() {
   return (
-    <div className="mx-auto w-full max-w-5xl py-3 px-3  md:py-3.5 md:px-4 ">
-      <div className="flex  flex-row   justify-between">
-        {/* <LemonIcon aria-hidden="true" /> */}
-        <div className="text-2xl  ml-1 font-bold">
-          <a href="/">Home</a>
-        </div>
+    <div className="hidden md:flex">
+      <div className="mx-auto w-full max-w-5xl py-3 px-3  md:py-3.5 md:px-4 ">
+        <div className="flex  flex-row   justify-between">
+          {/* <LemonIcon aria-hidden="true" /> */}
+          <div className="text-xl  ml-1 font-bold">
+            <a href="/">/~</a>
+          </div>
 
-        <div class="flex px-4 py-2">
-          {links.map((link) => (
-            <div>
-              <a
-                href={link.href}
-                class="justify-center p-4 text-gray-500 hover:text-black py-1 border-gray-500 [aria-current]:text-green-600"
-              >
-                {link.label}
-              </a>
-            </div>
-          ))}
-        </div>
-        <div>
-          <Button>
-            <a class="" href={asset("/resume.pdf")}>Download CV</a>
-          </Button>
-          {/* <ToggleTheme /> */}
+          <div class="flex px-4 py-2">
+            {links.map((link) => (
+              <div>
+                <a
+                  href={link.href}
+                  class="justify-center p-4 text-gray-500 hover:text-black py-1 border-gray-500 [aria-current]:text-green-600"
+                >
+                  {link.label}
+                </a>
+              </div>
+            ))}
+          </div>
+
+          <div>
+            <Button>
+              <a class="" href={asset("/resume.pdf")}>Download CV</a>
+            </Button>
+            {/* <ToggleTheme /> */}
+          </div>
         </div>
       </div>
     </div>
