4bb3c5d5095db8b83be2e5f2bffe1c31d29ccbbf
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-08-14 15:10:55 +0100

    pls fix

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -16,8 +16,7 @@ import * as $index from "./routes/index.tsx";
 import * as $projects from "./routes/projects.tsx";
 import * as $uses from "./routes/uses.tsx";
 import * as $Counter from "./islands/Counter.tsx";
-import * as $Dropdown from "./islands/Dropdown.tsx";
-import * as $ToggleTheme from "./islands/ToggleTheme.tsx";
+
 import { type Manifest } from "$fresh/server.ts";
 
 const manifest = {
@@ -38,8 +37,6 @@ const manifest = {
   },
   islands: {
     "./islands/Counter.tsx": $Counter,
-    "./islands/Dropdown.tsx": $Dropdown,
-    "./islands/ToggleTheme.tsx": $ToggleTheme,
   },
   baseUrl: import.meta.url,
 } satisfies Manifest;
