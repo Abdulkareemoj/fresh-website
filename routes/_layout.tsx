7a17c3d67c8de976456a4ef625ca86b74e842831
Abdulkareem Ojerinde <abdulkareemojerinde@gmail.com>
2024-08-14 15:22:20 +0100

    pls fix

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -1,13 +1,11 @@
 import { PageProps } from "$fresh/server.ts";
 import Footer from "../components/Footer.tsx";
 import Header from "../components/Header.tsx";
-import MobileNav from "../components/MobileNav.tsx";
 
-export default function Layout({ Component, state }: PageProps) {
+export default function Layout({ Component }: PageProps) {
   // do something with state here
   return (
     <>
-      <MobileNav />
       <Header />
       <main className="mx-auto max-w-3xl px-6 py-12 sm:px-6 sm:py-10 lg:py-10">
         <Component />
